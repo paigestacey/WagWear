e5fe7367d575aec044cd5013fdc25a420bac7007
Paige <gracepaigestacey@gmail.com>
2024-04-21 21:45:25 -0600

    dog api

--- script.js
+++ script.js
@@ -22,3 +22,38 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+/*API*/
+document.getElementById('getDogBreedBtn').addEventListener('click', function() {
+    fetch('https://api.thedogapi.com/v1/breeds', {
+      headers: {
+        'x-api-key': 'live_yD96x9FgQrnVaTxdEZ09eX36jEpTH4XcINkKVpFNFV20lbTdt1o0dVYMYyU2QfUG'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomBreed = data[randomIndex];
+  
+      document.getElementById('breedName').textContent = randomBreed.name;
+      document.getElementById('breedDescription').textContent = randomBreed.description;
+  
+      document.getElementById('dogBreedCard').style.display = 'block';
+
+      fetch(`https://api.thedogapi.com/v1/images/search?breed_id=${randomBreed.id}`, {
+        headers: {
+          'x-api-key': 'live_yD96x9FgQrnVaTxdEZ09eX36jEpTH4XcINkKVpFNFV20lbTdt1o0dVYMYyU2QfUG'
+        }
+      })
+
+      .then(response => response.json())
+    .then(imageData => {
+      const imageUrl = imageData[0].url;
+      const breedImage = document.getElementById('breedImage');
+      breedImage.src = imageUrl;
+      breedImage.style.display = 'block';
+    })
+    .catch(error => console.error('Error fetching image:', error));
+  })
+  .catch(error => console.error('Error fetching breed:', error));
+});
+
